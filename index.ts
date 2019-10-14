2cf01983b6ceb2a1a0893c3486c4623318f15bf6
transco901 <norbert.mahoney@gmail.com>
2019-10-14 09:17:43 -0500

    2 files modified

--- index.ts
+++ index.ts
@@ -1,50 +1,26 @@
 import { of, Observable } from 'rxjs'; 
 import { map, switchMap } from 'rxjs/operators';
 
+/***********************  map  ***********************/
 // Source observable.
+// of(): Converts the arguments to an observable sequence.
+// pipe(): takes as its arguments the functions you want to combine, and 
+// returns a new function that, when executed, runs the composed 
+// functions in sequence.
 
-const source = of('World', 'Houston', 'Dallas').pipe(
+// A set of operators applied to an observable is a recipe—that is, a 
+// set of instructions for producing the values you’re interested in. By 
+// itself, the recipe doesn’t do anything. You need to call subscribe() 
+// to produce a result through the recipe.
+
+const source = of('World', 'Houston', 'Flower Mound').pipe(
   map(x => `Hello ${x}!`) // transforming each value in the stream
 );
 
-// Subscribe/Listen and react
-//source.subscribe(x => console.log(x));
+// Subscribe/Listen
+source.subscribe(x => console.log(x));
 
 /***********************  map  ***********************/
 
 
-//const enrolledIds = enrollment.CurrentEnrollments.map((e) => e.CoverageOptionId);
-
-
-
-// const observable = new Observable(subscriber => {
-//   subscriber.next(1);
-//   subscriber.next(2);
-//   subscriber.next(3);
-//   setTimeout(() => {
-//     subscriber.next(4);
-//     subscriber.complete();
-//   }, 0);
-// });
-
-// console.log('just before subscribe');
-
-// observable.subscribe({
-//   next(x) { console.log('got value ' + x); },
-//   error(err) { console.error('something wrong ocurred: ' + err)},
-//   complete() { console.log('done')}
-// });
-
-// console.log('just after subscribe');
-
-//************GET THIS WORKING! */
-// observable.pipe(
-//   switchMap( () ) => console.log('value + 10 = {val}') )
-//   );
-
-
-//*********************** */
-//Observable.throw
-//
- //.catch((error: any) => Observable.throw({ message: 'Error occured when trying to get the data.' +        //error.message }));
 
