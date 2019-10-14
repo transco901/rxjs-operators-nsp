60954d24c230a3371976e7dc2560ec016fae6242
transco901 <norbert.mahoney@gmail.com>
2019-10-14 12:20:03 -0500

    More changes to map()

--- index.ts
+++ index.ts
@@ -1,44 +1,46 @@
-import { of, Observable } from 'rxjs'; 
+import { of, Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 
 /***********************  map  ***********************/
 
 // Source observable.
 // of(): Converts the arguments to an observable sequence.
-// pipe(): takes as its arguments the functions you want to combine, and 
-// returns a new function that, when executed, runs the composed 
+// pipe(): takes as its arguments the functions you want to combine, and
+// returns a new function that, when executed, runs the composed
 // functions in sequence.
 
-// A set of operators applied to an observable is a recipe—that is, a 
-// set of instructions for producing the values you’re interested in. By 
-// itself, the recipe doesn’t do anything. You need to call subscribe() 
+// A set of operators applied to an observable is a recipe—that is, a
+// set of instructions for producing the values you’re interested in. By
+// itself, the recipe doesn’t do anything. You need to call subscribe()
 // to produce a result through the recipe.
 
-// 1. basic example
-const source = of('World', 'Houston', 'Insperity').pipe(
-  map(x => `Hello ${x}!`) // transforming each value in the stream
-);
+/********* getting started *********/
+// const source = of('World', 'Houston', 'Insperity');
+
+// source.pipe( 
+//     map(x => `Hello ${x}!`) 
+//   );
 
 // Subscribe/Listen
-source.subscribe(x => console.log(x));
+//source.subscribe(x => console.log(x));
 
-// 2. use case
-class Employee {
-  name: string;
-  perId: number;
-}
+/********* transform data *********/
+// class Employee {
+//   name: string;
+//   perId: number;
+// }
 
-var employee: Employee;
+// var employee: Employee;
 
-function getEmployee() {
-  return of({name: 'Maria Gomez', perId:3211});
-}
+// function getEmployee() {
+//   return of({name: 'Maria Gomez', perId:3211});
+// }
 
-getEmployee().pipe(
-  map(ee => { 
-    employee = ee;
-    console.log(`Employee mapped: ${employee.name}, ${employee.perId}`);
-  })).subscribe();
+// getEmployee().pipe(
+//   map(ee => {
+//     employee = ee;
+//     console.log(`Employee mapped: ${employee.name}, ${employee.perId}`);
+//   })).subscribe();
 
 /***********************  switchMap  ***********************/
 
