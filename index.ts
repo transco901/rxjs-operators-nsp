938f1946e2939d91fc101ba18d823158b5c72c9e
transco901 <norbert.mahoney@gmail.com>
2019-10-15 12:24:43 -0500

    3 files modified

--- index.ts
+++ index.ts
@@ -81,11 +81,22 @@ import { ajax } from 'rxjs/ajax';
 // ).subscribe(x => console.log(x));
 
 /****************************  switchMap  ****************************/
-// Polling - every time service is called a new Observable is created and old is
-// cancelled.
-var mockDataIndex = 
-const mockData = ['data1', 'data2'];
-function getUpdatedData() {
-  return of()
+// type ahead - only interested in latest input; cancel the rest.
+
+
+function filterSearchItems() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
-var pollingSubscription = interval(2000).switchMap(()=> getUpdatedData())
