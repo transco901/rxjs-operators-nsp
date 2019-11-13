1586a9af75a7fdb53da53a6d1fb488c5a5a117bc
transco901 <norbert.mahoney@gmail.com>
2019-11-12 21:26:56 -0600

    2 files modified

--- index.ts
+++ index.ts
@@ -13,64 +13,65 @@ import {
 } from "rxjs/operators";
 
 /****************************  map and filter ****************************/
-console.clear();
+// console.clear();
 
-class Employee {
-  name: string;
-  perId: number;
-  companyId: number;
-  age: number;
-}
+// class Employee {
+//   name: string;
+//   perId: number;
+//   companyId: number;
+//   age: number;
+// }
 
-function transformToEmployee(ee: any): Employee {
-  const newEE = new Employee();  
-  newEE.name = ee.name;
-  newEE.perId = ee.perId;
-  newEE.companyId = ee.companyId;
-  newEE.age = getAge(ee.birthDate);
-  return newEE;
-}
+// function transformToEmployee(ee: any): Employee {
+//   const newEE = new Employee();  
+//   newEE.name = ee.name;
+//   newEE.perId = ee.perId;
+//   newEE.companyId = ee.companyId;
+//   newEE.age = getAge(ee.birthDate);
+//   return newEE;
+// }
 
-function getAge(birthDate: Date) {
-    var today = new Date();
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-    return age;
-}
+// function getAge(birthDate: Date) {
+//     var today = new Date();
+//     var age = today.getFullYear() - birthDate.getFullYear();
+//     var m = today.getMonth() - birthDate.getMonth();
+//     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+//         age--;
+//     }
+//     return age;
+// }
 
-let employees = new Array<Employee>();
+// let employees = new Array<Employee>();
 
-function getEmployees() {  
-  let employees = [
-    { name: "Ben Jones", perId: 3211, companyId: 8100, birthDate: new Date(1984, 11, 1) },
-    { name: "John Barnaby", perId: 4212, companyId: 9200, birthDate: new Date(2000, 11, 1) },
-    { name: "Joyce Barnaby", perId: 4213, companyId: 8100, birthDate: new Date(1987, 1, 1) },
-    { name: "Charlie Nelson", perId: 5213, companyId: 9200, birthDate: new Date(2001, 4, 4) },
-    { name: "Kate Wilding", perId: 5214, companyId: 9200, birthDate: new Date(1993, 10, 12) },
-    { name: "Gavin Troy", perId: 5215, companyId: 8100, birthDate: new Date(1977, 1, 19) }
-  ];
+// function getEmployees() {  
+//   let employees = [
+//     { name: "Ben Jones", perId: 3211, companyId: 8100, birthDate: new Date(1984, 11, 1) },
+//     { name: "John Barnaby", perId: 4212, companyId: 9200, birthDate: new Date(2000, 11, 1) },
+//     { name: "Joyce Barnaby", perId: 4213, companyId: 8100, birthDate: new Date(1987, 1, 1) },
+//     { name: "Charlie Nelson", perId: 5213, companyId: 9200, birthDate: new Date(2001, 4, 4) },
+//     { name: "Kate Wilding", perId: 5214, companyId: 9200, birthDate: new Date(1993, 10, 12) },
+//     { name: "Gavin Troy", perId: 5215, companyId: 8100, birthDate: new Date(1977, 1, 19) }
+//   ];
 
-  return from(employees)
-              .pipe(concatMap(ee => of(ee).pipe(delay(500))));
-}
+//   return from(employees)
+//               .pipe(concatMap(ee => of(ee).pipe(delay(500))));
+// }
 
-// pipe(): takes as its arguments the functions you want to combine (a.k.a. a recipe), and returns a new function that, when executed, runs the composed functions in sequence
-getEmployees()
-  .pipe(
-    map(ee => {
-      const employee: Employee = transformToEmployee(ee);
-      employees.push(employee);
-      console.log(`Name: ${employee.name}, id: ${employee.perId}, age: ${employee.age}`)
-    })
-  );
-  // subscribe() adds an observer which triggers the 'lazy' observable to start pushing values
-  //.subscribe();
+// // pipe(): takes as its arguments the functions you want to combine (a.k.a. a recipe), and returns a new function that, when executed, runs the composed functions in sequence
+// getEmployees()
+//   .pipe(
+//     map(ee => {
+//       const employee: Employee = transformToEmployee(ee);
+//       employees.push(employee);
+//       console.log(`Name: ${employee.name}, id: ${employee.perId}, age: ${employee.age}`)
+//     })
+//   );
+//   // subscribe() adds an observer which triggers the 'lazy' observable to start pushing values
+//   //.subscribe();
 
 
-/************************  switchMap ************************/
+/***********************************  switchMap ***********************************/
+
 // console.clear();
 // // switchMap() - type ahead search - only interested in latest input; cancel the rest.
 // // debounceTime() - delay and only the most recent value from each burst of emissions.  wait for the user input to stabilize
@@ -102,3 +103,22 @@ getEmployees()
 // }
 
 // document.getElementById("divForSearchBox").style.visibility = 'visible';
+
+/***********************************  concatMap ***********************************/
+console.clear();
+
+const deleteButtons = document.getElementsByClassName('deleteBtn');
+
+const source = fromEvent(deleteButtons, 'click').pipe(
+  concatMap(e => fakeDeleteHttpRequest(e) ));
+
+source.subscribe();
+
+function fakeDeleteHttpRequest(e: Event) {
+  const button = e.target as HTMLButtonElement;
+  button.id
+
+  return of(e);
+}
+document.getElementById("divForConcatMap").style.visibility = 'visible';
+
