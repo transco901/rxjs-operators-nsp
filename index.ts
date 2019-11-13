a309970cbff81a0b48fc2381f19ce5771358635f
transco901 <norbert.mahoney@gmail.com>
2019-11-13 07:17:15 -0600

    1 file modified

--- index.ts
+++ index.ts
@@ -13,61 +13,61 @@ import {
 } from "rxjs/operators";
 
 /****************************  map and filter ****************************/
-// console.clear();
+console.clear();
 
-// class Employee {
-//   name: string;
-//   perId: number;
-//   companyId: number;
-//   age: number;
-// }
+class Employee {
+  name: string;
+  perId: number;
+  companyId: number;
+  age: number;
+}
 
-// function transformToEmployee(ee: any): Employee {
-//   const newEE = new Employee();  
-//   newEE.name = ee.name;
-//   newEE.perId = ee.perId;
-//   newEE.companyId = ee.companyId;
-//   newEE.age = getAge(ee.birthDate);
-//   return newEE;
-// }
+function transformToEmployee(ee: any): Employee {
+  const newEE = new Employee();  
+  newEE.name = ee.name;
+  newEE.perId = ee.perId;
+  newEE.companyId = ee.companyId;
+  newEE.age = getAge(ee.birthDate);
+  return newEE;
+}
 
-// function getAge(birthDate: Date) {
-//     var today = new Date();
-//     var age = today.getFullYear() - birthDate.getFullYear();
-//     var m = today.getMonth() - birthDate.getMonth();
-//     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-//         age--;
-//     }
-//     return age;
-// }
+function getAge(birthDate: Date) {
+    var today = new Date();
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
 
-// let employees = new Array<Employee>();
+let employees = new Array<Employee>();
 
-// function getEmployees() {  
-//   let employees = [
-//     { name: "Ben Jones", perId: 3211, companyId: 8100, birthDate: new Date(1984, 11, 1) },
-//     { name: "John Barnaby", perId: 4212, companyId: 9200, birthDate: new Date(2000, 11, 1) },
-//     { name: "Joyce Barnaby", perId: 4213, companyId: 8100, birthDate: new Date(1987, 1, 1) },
-//     { name: "Charlie Nelson", perId: 5213, companyId: 9200, birthDate: new Date(2001, 4, 4) },
-//     { name: "Kate Wilding", perId: 5214, companyId: 9200, birthDate: new Date(1993, 10, 12) },
-//     { name: "Gavin Troy", perId: 5215, companyId: 8100, birthDate: new Date(1977, 1, 19) }
-//   ];
+function getEmployees() {  
+  let employees = [
+    { name: "Ben Jones", perId: 3211, companyId: 8100, birthDate: new Date(1984, 11, 1) },
+    { name: "John Barnaby", perId: 4212, companyId: 9200, birthDate: new Date(2000, 11, 1) },
+    { name: "Joyce Barnaby", perId: 4213, companyId: 8100, birthDate: new Date(1987, 1, 1) },
+    { name: "Charlie Nelson", perId: 5213, companyId: 9200, birthDate: new Date(2001, 4, 4) },
+    { name: "Kate Wilding", perId: 5214, companyId: 9200, birthDate: new Date(1993, 10, 12) },
+    { name: "Gavin Troy", perId: 5215, companyId: 8100, birthDate: new Date(1977, 1, 19) }
+  ];
 
-//   return from(employees)
-//               .pipe(concatMap(ee => of(ee).pipe(delay(500))));
-// }
+  return from(employees)
+              .pipe(concatMap(ee => of(ee).pipe(delay(500))));
+}
 
-// // pipe(): takes as its arguments the functions you want to combine (a.k.a. a recipe), and returns a new function that, when executed, runs the composed functions in sequence
-// getEmployees()
-//   .pipe(
-//     map(ee => {
-//       const employee: Employee = transformToEmployee(ee);
-//       employees.push(employee);
-//       console.log(`Name: ${employee.name}, id: ${employee.perId}, age: ${employee.age}`)
-//     })
-//   );
-//   // subscribe() adds an observer which triggers the 'lazy' observable to start pushing values
-//   //.subscribe();
+// pipe(): takes as its arguments the functions you want to combine (a.k.a. a recipe), and returns a new function that, when executed, runs the composed functions in sequence
+getEmployees()
+  .pipe(
+    map(ee => {
+      const employee: Employee = transformToEmployee(ee);
+      employees.push(employee);
+      console.log(`Name: ${employee.name}, id: ${employee.perId}, age: ${employee.age}`)
+    })
+  );
+  // subscribe() adds an observer which triggers the 'lazy' observable to start pushing values
+  //.subscribe();
 
 
 /***********************************  switchMap ***********************************/
@@ -105,26 +105,26 @@ import {
 // document.getElementById("divForSearchBox").style.visibility = 'visible';
 
 /***********************************  concatMap ***********************************/
-console.clear();
+// console.clear();
 
-const deleteButtons = document.getElementsByClassName('deleteBtn');
+// const deleteButtons = document.getElementsByClassName('deleteBtn');
 
-const source = fromEvent(deleteButtons, 'click').pipe(
-  concatMap(e => fakeDeleteHttpRequest(e) )  
-  //switchMap(e => fakeDeleteHttpRequest(e) )  // demonstrate how switchMap will not wait.
-);
+// const source = fromEvent(deleteButtons, 'click').pipe(
+//   concatMap(e => fakeDeleteHttpRequest(e) )  
+//   //switchMap(e => fakeDeleteHttpRequest(e) )  // demonstrate how switchMap will not wait.
+// );
 
-source.subscribe(button => {
-  console.log(`%cAPI Call Complete: ${button.nextElementSibling.textContent}`, 'color: green');
-  button.parentElement.parentElement.remove();
-});
+// source.subscribe(button => {
+//   console.log(`%cAPI Call Complete: ${button.nextElementSibling.textContent}`, 'color: green');
+//   button.parentElement.style.color = 'red';
+// });
 
-function fakeDeleteHttpRequest(e: Event) {
-  const button = e.target as HTMLButtonElement;
-    console.log(`%cAPI Call Requested: ${button.nextElementSibling.textContent}`, 'color: orange');
-  return of(button).pipe(delay( Number(button.id) ));
-}
+// function fakeDeleteHttpRequest(e: Event) {
+//   const button = e.target as HTMLButtonElement;
+//     console.log(`%cAPI Call Requested: ${button.nextElementSibling.textContent}`, 'color: orange');
+//   return of(button).pipe(delay( Number(button.id) ));
+// }
 
-document.getElementById("divForConcatMap").style.visibility = 'visible';
+// document.getElementById("divForConcatMap").style.visibility = 'visible';
 
 /***********************************  Error handling ***********************************/
